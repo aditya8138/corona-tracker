688d6574f094f31b2fd84fef8b3a08d36365e751
Aditya <aditya.singh@modopayments.com>
2020-04-19 16:26:10 -0500

    Updates

---
+++ precache-manifest.8ff9f188093c80b1584241977a85dd99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec94758fca07dbad19956b95e975e6b",
+    "url": "/coronaTracker/index.html"
+  },
+  {
+    "revision": "f9214938e94ef6d8e7e9",
+    "url": "/coronaTracker/static/css/main.50f70d73.chunk.css"
+  },
+  {
+    "revision": "8de030ef479ad92fa13a",
+    "url": "/coronaTracker/static/js/2.cb771155.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronaTracker/static/js/2.cb771155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9214938e94ef6d8e7e9",
+    "url": "/coronaTracker/static/js/main.f0c9ecdf.chunk.js"
+  },
+  {
+    "revision": "c88431133388ca520e75",
+    "url": "/coronaTracker/static/js/runtime-main.a776e5b8.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronaTracker/static/media/image.d7265326.png"
+  }
+]);
